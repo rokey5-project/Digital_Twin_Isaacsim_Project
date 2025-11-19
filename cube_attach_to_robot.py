b2de23a3055bcb648442ed824cbbc5de00265248
gabmin <rlarkqals@gmail.com>
2025-11-19 20:33:55 +0900

    적용 수치 일부 수정 및 불필요한 코드 제거

--- cube_attach_to_robot.py
+++ cube_attach_to_robot.py
@@ -1,18 +1,13 @@
 import omni.usd
 from pxr import UsdGeom, Sdf, Gf, Usd, UsdPhysics
-import omni.kit.app
 from omni.isaac.dynamic_control import _dynamic_control
-from sensor_msgs.msg import JointState
+
 
 dc = _dynamic_control.acquire_dynamic_control_interface()
 stage = omni.usd.get_context().get_stage()
 
-
 CUBE_PATH = "/World/Cube"
 BASE_LINK_PATH = "/World/Nova_Carter_ROS/chassis_link/base_link"
-
-print("Cube:", CUBE_PATH)
-print("Base:", BASE_LINK_PATH)
 
 def get_world_position(prim_path):
     prim = stage.GetPrimAtPath(prim_path)
@@ -24,7 +19,7 @@ def get_world_position(prim_path):
 
 attached = False
 
-def on_update(dt):
+def on_update():
     global attached
 
     robot_pos = get_world_position(BASE_LINK_PATH)
@@ -35,31 +30,20 @@ def on_update(dt):
         return
 
     dist = (cube_pos - robot_pos).GetLength()
-    print("dist:", dist, attached)
 
-    if (not attached) and dist < 1:
+    if (not attached) and dist < 1.5:
         print("📦 Attaching cube!")
 
         old = Sdf.Path(CUBE_PATH)
         new = Sdf.Path(BASE_LINK_PATH)
-
-        print(old, new)
-
-        # joint_props = _dynamic_control.D6JointProperties()
-        # joint_props.name = "robot_cube_joint"
-        # joint_props.body0 = old   # 부모 prim
-        # joint_props.body1 = new  # 자식 prim
-
         
         joint_prim = UsdPhysics.FixedJoint.Define(stage, '/World/Cube/Joints')
         joint_prim.CreateBody0Rel().SetTargets([new])
         joint_prim.CreateBody1Rel().SetTargets([old])
 
-        joint_prim.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 0.0, 0.6))
+        joint_prim.CreateLocalPos0Attr().Set(Gf.Vec3f(-0.2, 0.0, 1.0))
 
         attached = True
 
 
-
 subscription = omni.kit.app.get_app().get_update_event_stream().create_subscription_to_pop(on_update)
-print("✔ Distance-based attach system activated!")
